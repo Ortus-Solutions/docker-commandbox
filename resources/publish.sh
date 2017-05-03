9da5532b98ac94be26ed1ca0667ab236ddcd9bd6
Jon Clausen <jon_clausen@silowebworks.com>
2017-05-03 13:45:49 -0400

    fix tag appendices for dev branches

--- resources/publish.sh
+++ resources/publish.sh
@@ -8,16 +8,21 @@ echo "INFO: Docker image successfully built"
 docker login -u $DOCKER_HUB_USERNAME -p $DOCKER_HUB_PASSWORD
 echo "INFO: Successfully logged in to Docker Hub!"
 
-if [[ $TRAVIS_BRANCH == 'development' ]]; then
-	BUILD_IMAGE_TAG="${BUILD_IMAGE_TAG}-snapshot"
-fi
-
 # Tag our image with the build reference
-docker tag ${TRAVIS_COMMIT}:${TRAVIS_JOB_ID} ${BUILD_IMAGE_TAG}
-
 # Add :latest tag, if applicable
 if [[ ${BUILD_IMAGE_TAG} == 'ortussolutions/commandbox' ]] && [[ $TRAVIS_BRANCH == 'master' ]]; then
+	docker tag ${TRAVIS_COMMIT}:${TRAVIS_JOB_ID} ${BUILD_IMAGE_TAG}
+	docker tag ${TRAVIS_COMMIT}:${TRAVIS_JOB_ID} ${BUILD_IMAGE_TAG}:${COMMANDBOX_VERSION}
     docker tag ${TRAVIS_COMMIT}:${TRAVIS_JOB_ID} ${BUILD_IMAGE_TAG}:latest
+else
+
+	if [[ ${BUILD_IMAGE_TAG} == 'ortussolutions/commandbox' ]] && [[ $TRAVIS_BRANCH == 'development' ]]; then
+		BUILD_IMAGE_TAG="${BUILD_IMAGE_TAG}:snapshot"
+	elif [[ $TRAVIS_BRANCH == 'development' ]]; then
+		BUILD_IMAGE_TAG="${BUILD_IMAGE_TAG}-snapshot"
+	fi
+
+	docker tag ${TRAVIS_COMMIT}:${TRAVIS_JOB_ID} ${BUILD_IMAGE_TAG}
 fi
 
 # Push our new image and tags to the registry
@@ -27,6 +32,7 @@ docker push ${BUILD_IMAGE_TAG}
 
 if [[ ${BUILD_IMAGE_TAG} == 'ortussolutions/commandbox' ]] && [[ $TRAVIS_BRANCH == 'master' ]]; then
     docker push ${BUILD_IMAGE_TAG}:latest
+    docker push ${BUILD_IMAGE_TAG}:${COMMANDBOX_VERSION}
 fi
 
 echo "INFO: Image ${BUILD_IMAGE_TAG} successfully published"
