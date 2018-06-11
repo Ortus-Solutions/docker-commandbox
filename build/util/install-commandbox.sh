8b6eb8ab21c0bec565b7c333daaf27eb28b975c3
Jon Clausen <jon_clausen@silowebworks.com>
2018-06-11 19:24:05 -0400

    fix version in path

--- build/util/install-commandbox.sh
+++ build/util/install-commandbox.sh
@@ -6,6 +6,6 @@
 # curl --location 'https://www.ortussolutions.com/parent/download/commandbox/type/bin' -o /tmp/box.zip
 
 # Using snapshot to ensure character encoding fix
-curl --location 'http://integration.stg.ortussolutions.com/artifacts/ortussolutions/commandbox/4.0.0-SNAPSHOT/commandbox-bin-4.1.0-SNAPSHOT.zip' -o /tmp/box.zip
+curl --location 'http://integration.stg.ortussolutions.com/artifacts/ortussolutions/commandbox/4.1.0-SNAPSHOT/commandbox-bin-4.1.0-SNAPSHOT.zip' -o /tmp/box.zip
 unzip /tmp/box.zip -d ${BIN_DIR} && chmod +x ${BIN_DIR}/box
 echo "$(box version) successfully installed"
