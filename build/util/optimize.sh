317152f735ec980b43573c410e5c370caf29fb9c
Jon Clausen <jon_clausen@silowebworks.com>
2018-06-16 09:13:35 -0400

    swap out CLI Lucee with light version and add additional cleanup routines

--- build/util/optimize.sh
+++ build/util/optimize.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
 # Cleanup CommandBox modules which would not be necessary in a Container environment
-UI_MODULES=( "coldbox" "contentbox" "cachebox" "forgebox" "logbox" "games" "wirebox" )
+SYSTEM_EXCLUDES=( "coldbox" "contentbox" "cachebox" "forgebox" "logbox" "games" "wirebox" )
+MODULE_EXCLUDES=( "cfscriptme-command" "cb-module-template" )
 
-for mod in "${UI_MODULES[@]}"
+for mod in "${SYSTEM_EXCLUDES[@]}"
 do
 	rm -rf /root/.CommandBox/cfml/system/modules_app/${mod}-commands
+done
+
+for mod in "${MODULE_EXCLUDES[@]}"
+do
+	rm -rf /root/.CommandBox/cfml/modules/${mod}
 done
 
 # Remove any engine artifacts
@@ -14,5 +20,12 @@ rm -rf /root/.CommandBox/server/*
 rm -rf /root/.CommandBox/temp/*
 # Remove any log files
 rm -rf /root/.CommandBox/logs/*
+# Remove cachebox caches
+rm -rf /root/.CommandBox/cfml/system/mdCache/*
+# Remove the felix cache
+rm -rf /root/.CommandBox/engine/cfml/cli/lucee-server/felix-cache/*
+# Remove jGit jars as the commands have been removed
+rm -rf /root/.CommandBox/lib/org.eclipse.jgit-*.jar
+rm -rf /root/.CommandBox/lib/ortus-jgit.jar
 # Clear downloaded artificacts
 box artifacts clean --force
