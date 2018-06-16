fcc01e5b760a4e8db07ad458b7bc0919a0e2b919
Jon Clausen <jon_clausen@silowebworks.com>
2018-06-16 09:38:13 -0400

    remove light version swapout since modules are still being downloaded at runtime

--- build/util/install-commandbox.sh
+++ build/util/install-commandbox.sh
@@ -5,9 +5,3 @@ curl --location 'https://www.ortussolutions.com/parent/download/commandbox/type/
 
 unzip /tmp/box.zip -d ${BIN_DIR} && chmod +x ${BIN_DIR}/box
 echo "$(box version) successfully installed"
-
-# Swap out the full lucee jar with a slimmed down version without modules to reduce our size
-rm -rf /root/.CommandBox/lib/lucee-*.jar
-# We have to hard-code our lucee download version until a generic download link is provided
-# We name it without the version so that the warmed up Lucee server will download a completely new version with the extensions
-curl --location 'http://cdn.lucee.org/lucee-light-5.2.7.63.jar' -o /root/.CommandBox/lib/lucee-light.jar
