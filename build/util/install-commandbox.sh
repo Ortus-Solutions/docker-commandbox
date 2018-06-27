fbb1c0ca77e478939ab15c03a36b6606eb8b2846
Jon Clausen <jon_clausen@silowebworks.com>
2018-06-27 07:12:36 -0400

    argument updates to CommandBox install

--- build/util/install-commandbox.sh
+++ build/util/install-commandbox.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Installs the latest CommandBox Binary
-curl -k -location "https://downloads.ortussolutions.com.s3.amazonaws.com/ortussolutions/commandbox/${COMMANDBOX_VERSION}/commandbox-bin-${COMMANDBOX_VERSION}.zip" -o /tmp/box.zip
-
+mkdir -p /tmp
+curl -k  -o /tmp/box.zip -location "https://downloads.ortussolutions.com.s3.amazonaws.com/ortussolutions/commandbox/${COMMANDBOX_VERSION}/commandbox-bin-${COMMANDBOX_VERSION}.zip"
 unzip /tmp/box.zip -d ${BIN_DIR} && chmod +x ${BIN_DIR}/box
 echo "$(box version) successfully installed"
