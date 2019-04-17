863fb20247e7a2fb593de92396bdcea3bfc69e7c
Joe Gooch <mrwizard@sapphirek12.com>
2019-04-17 09:22:43 -0400

    Ensure mode 755 explicitly especially because -rw+x isn't enough to run the binary, since commandbox needs to read itself for the payload to unzip things.

--- build/util/install-commandbox.sh
+++ build/util/install-commandbox.sh
@@ -11,7 +11,7 @@ set -ex
 # Installs the latest CommandBox Binary
 mkdir -p /tmp
 curl -k  -o /tmp/box.zip -location "https://downloads.ortussolutions.com/ortussolutions/commandbox/${COMMANDBOX_VERSION}/commandbox-bin-${COMMANDBOX_VERSION}.zip"
-unzip /tmp/box.zip -d ${BIN_DIR} && chmod +x ${BIN_DIR}/box
+unzip /tmp/box.zip -d ${BIN_DIR} && chmod 755 ${BIN_DIR}/box
 echo "$(box version) successfully installed"
 
 # Cleanup CommandBox modules which would not be necessary in a Container environment
