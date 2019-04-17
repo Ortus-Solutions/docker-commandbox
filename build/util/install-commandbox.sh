775846e1484d3952097f91e28d0061d2a75beeb0
Joe Gooch <mrwizard@sapphirek12.com>
2019-04-17 09:22:05 -0400

    Add variable safety Add set -e so that errors immediately fail the script, instead of attempting to continue on. Add set -x to show commands as they're being run. (that's my preference, could be removed.)

--- build/util/install-commandbox.sh
+++ build/util/install-commandbox.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+if [ -z "$COMMANDBOX_VERSION" ]; then
+  echo "CommandBox Version not supplied via variable COMMANDBOX_VERSION"
+  exit 1
+fi
+
+# Make sure errors (like curl failing, or unzip failing, or anything failing) fails the build
+set -ex
+
 # Installs the latest CommandBox Binary
 mkdir -p /tmp
 curl -k  -o /tmp/box.zip -location "https://downloads.ortussolutions.com/ortussolutions/commandbox/${COMMANDBOX_VERSION}/commandbox-bin-${COMMANDBOX_VERSION}.zip"
