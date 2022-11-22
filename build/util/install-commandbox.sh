ab01fa3bfb06b97ded4ba3beca1e10c21a64cd63
Jonathan Clausen <jclausen@ortussolutions.com>
2022-11-22 09:45:20 -0500

    remove box.zip after unzipping

--- build/util/install-commandbox.sh
+++ build/util/install-commandbox.sh
@@ -11,7 +11,7 @@ fi
 # Installs the latest CommandBox Binary
 mkdir -p /tmp
 curl -k  -o /tmp/box.zip -location "https://downloads.ortussolutions.com/ortussolutions/commandbox/${COMMANDBOX_VERSION}/commandbox-bin-${COMMANDBOX_VERSION}.zip"
-unzip /tmp/box.zip -d ${BIN_DIR} && chmod 755 ${BIN_DIR}/box
+unzip /tmp/box.zip -d ${BIN_DIR} && chmod 755 ${BIN_DIR}/box && rm -f /tmp/box.zip
 echo "commandbox_home=${COMMANDBOX_HOME}" > ${BIN_DIR}/commandbox.properties
 
 echo "$(box version) successfully installed"
